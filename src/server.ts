1c27e4cc7babfc3fcaf6a9e5e896edf034f57d3b
italo santiago <italo.miranda8635@gmail.com>
2024-05-03 00:29:47 -0300

    criado sistema de criação de salas e envio de mensagens apenas para usuarios da sala

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import http from 'http'
-import { Server } from "socket.io"
+import { Server, Socket } from "socket.io"
 
 const server = express()
 
@@ -13,7 +13,7 @@ const io = new Server(httpServer)
 server.get('/', (req, res)=>{
     res.sendFile(__dirname + '/index.html')
 })
-
+/*
 io.on('connection', (socket)=>{
     console.log('user connected =>', socket.id)
 
@@ -22,6 +22,52 @@ io.on('connection', (socket)=>{
         io.emit('message', msg)
     })
 })
+*/
+// parte da ideia do chhatGPT
+
+const socketIdToUser = {}
+
+const roomParticipants = {}
+
+io.on('connection', (socket)=>{
+    socket.on('setUserId', ()=>{
+        socketIdToUser[socket.id] = socket.id
+        socket.emit("IdUser", socket.id)
+        console.log("ID do user", socket.id)
+    })
+
+    socket.on('createRoom', ()=>{
+        if (socketIdToUser[socket.id]) {
+            const roomId = Math.random().toString(36).substring(2,9)
+        
+            socket.join(roomId)
+    
+            roomParticipants[roomId] = [socketIdToUser[socket.id]]
+            socket.emit('roomCreated', roomId);
+            console.log('room creates', roomParticipants)
+        }else{
+
+        }
+
+    })
+
+    socket.on('joinRoom', (roomId)=>{
+
+        if (roomParticipants[roomId]) {
+            socket.join(roomId)
+            socket.emit('joinRoom', (roomId))
+        }else{
+            socket.emit('joinRoom', false)
+        }
+    })
+
+    socket.on('message', (msg, roomId)=>{
+        console.log(msg, roomId)
+        io.to(roomId).emit('messageReceived', {userId: socketIdToUser[socket.id], msg})
+    })
+})
+
+//
 
 //iniciar server
 httpServer.listen(3000, ()=> console.log('server is running'))
